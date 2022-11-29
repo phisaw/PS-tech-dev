a0aec1c7073689ea0c5a35af6f09977c16cce645
phisaw <psawallies@gmail.com>
2022-11-29 21:14:51 +0000

    counter fixed

--- modules/counter.js
+++ modules/counter.js
@@ -9,9 +9,26 @@ btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         const styles = e.currentTarget.classList
         
-        if(styles.contains('increase')) {
+        if(styles.contains('Increase')) {
             count++
+        }else 
+        if (styles.contains('Decrease')) {
+            count--
+        }else {
+            count = 0
         }
+
+
+        if (count > 0) {
+            counter.style.color = 'green'
+        }else
+        if (count < 0) {
+            counter.style.color = 'red'
+        }
+        else {
+            counter.style.color ='black'
+        }
+
         counter.textContent = count
     })
 })
