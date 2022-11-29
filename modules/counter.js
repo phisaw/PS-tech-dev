3fac89bc653946b6d3dda91ed5179068334d6c1d
phisaw <psawallies@gmail.com>
2022-11-29 19:58:30 +0000

    buttons counter

---
+++ modules/counter.js
@@ -0,0 +1,17 @@
+const counter = document.querySelector('#counter')
+const btns = document.querySelectorAll('.btn')
+
+console.log(btns)
+
+let count = 0
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const styles = e.currentTarget.classList
+        
+        if(styles.contains('increase')) {
+            count++
+        }
+        counter.textContent = count
+    })
+})
