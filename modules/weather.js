e87f013d6d1a93abf52575619846b8b652bfffe6
phisaw <psawallies@gmail.com>
2022-12-06 19:10:17 +0000

    js loops

--- modules/weather.js
+++ modules/weather.js
@@ -22,4 +22,95 @@ function sum(a,b) {
     
 }
 
-console.log(sum(2,4))
+//console.log(sum(2,4))
+
+
+function calcFoodTot(food, tip) {
+    const tipPercentage = tip /100
+    const tipAmount = food *tipPercentage
+    const total = sum(food, tipAmount)
+    return total
+    
+}
+
+//console.log(calcFoodTot(150,20))
+
+const sumArrow = (a,b) => a + b
+//console.log(sumArrow(10,50))
+
+
+// groceries = ['banana',"apple","pear","orange" ]
+
+// groceries.push('toe')
+// //console.log(groceries.slice(1,3))
+// //console.log(groceries.indexOf('apple'))
+
+// const person = {
+//     name: 'Leonardo',
+//     shirt: 'white'
+// }
+// const person2 = {
+//     name: 'Amber',
+//     shirt: 'Blue'
+// }
+const introducer = (name, shirt) => {
+    const person = {
+        name: name,
+        shirt: shirt,
+        assets: 100000,
+        liability: 50000,
+        networth: function () {
+            return this.assets - this.liability
+        }
+    }
+    const intro = `Hi my name is ${person.name} and the color of my shirt is ${person.shirt}. My networth is $${person.networth()} USD`
+    return intro
+}
+
+//console.log(introducer('Leonardo','White '))
+
+
+fruits = ['banana',"apple","pear","orange" ]
+
+
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i])
+// }
+// for (const fruit of fruits) {
+//     console.log('hello ' + fruit)
+// }
+
+const numbers = [1,2,3,4,5,6,7,8]
+
+let result = []
+
+// for (const nums of numbers) {
+//     console.log(nums*2)
+//     result.push(nums * 2)
+// }
+
+//console.log(result)
+
+const double = (numbers) => {
+    let result = []
+    for (const number of numbers) {
+        result.push(number ** 2)
+    }
+    return result
+}
+//console.log(double([1,2,3,4,5,6]))
+
+const letterCounter = () => {
+
+    let result = 0;
+
+    for (index in phrase) {
+        console.log(Number(index)+1)
+        result = Number(index) + 1
+    }
+    return {result}
+}
+
+
+const phrase = prompt('Letters in this sentence is')
+letterCounter()
