887ab410d1f983f526d126cbf22c2898a4443799
phisaw <psawallies@gmail.com>
2022-12-18 18:07:53 +0000

    weatherApp search done

--- modules/weatherApp.js
+++ modules/weatherApp.js
@@ -13,29 +13,40 @@ const temp = document.getElementById('temp')
 const feelsLike = document.getElementById('feelsLike')
 const imgContainer = document.getElementById('weatherImage_container')
 const wind = document.getElementById('wind')
-
 const lastUpdate = document.getElementById('lastUpdate')
 
 
-fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=gothenburg', options)
+const searchCity = document.getElementById('weather_input')
+const submitCity = document.getElementById('weather_btn')
 
-	.then(response => response.json())
-	.then(json => {
-		
-		console.log(json)
-		cityName.innerText = json.location.name
-		weatherCond.innerText = json.current.condition.text
-		temp.innerText = json.current.temp_c
-		feelsLike.innerText = json.current.feelslike_c
-		wind.innerText = json.current.wind_kph
 
-		imgContainer.innerHTML = `<img src='${json.current.condition.icon}'/>`
-		lastUpdate.innerText = json.current.last_updated
-	}
-	)
-		
-	// .catch(err => console.error(err));
+
+
+submitCity.onclick = () => getWeather(searchCity.value)
+
+
+const getWeather = (cityWeather) => {
+fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${cityWeather}`, options)
+
+.then(response => response.json())
+.then(json => {
 	
-		
+	console.log(json)
+	cityName.innerText = json.location.name
+	weatherCond.innerText = json.current.condition.text
+	temp.innerText = json.current.temp_c
+	feelsLike.innerText = json.current.feelslike_c
+	wind.innerText = json.current.wind_kph
 
+	imgContainer.innerHTML = `<img src='${json.current.condition.icon}'/>`
+	lastUpdate.innerText = json.current.last_updated
+}
+)
+}
 
+searchCity.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      // Submit the form
+      getWeather(searchCity.value)
+    }
+  });
