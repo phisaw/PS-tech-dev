6d218141eb03997f27fc09d48d8ee62eb3d213a0
phisaw <psawallies@gmail.com>
2022-12-18 15:22:06 +0000

    static weather done

--- modules/weatherApp.js
+++ modules/weatherApp.js
@@ -6,13 +6,36 @@ const options = {
 	}
 };
 
+
+const cityName = document.getElementById('city')
+const weatherCond = document.getElementById('weatherCondition')
+const temp = document.getElementById('temp')
+const feelsLike = document.getElementById('feelsLike')
+const imgContainer = document.getElementById('weatherImage_container')
+const wind = document.getElementById('wind')
+
+const lastUpdate = document.getElementById('lastUpdate')
+
+
 fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=gothenburg', options)
-    // const temp = response.current
+
 	.then(response => response.json())
-	.then(response => current = console.log(response))
-    
-	.catch(err => console.error(err));
+	.then(json => {
+		
+		console.log(json)
+		cityName.innerText = json.location.name
+		weatherCond.innerText = json.current.condition.text
+		temp.innerText = json.current.temp_c
+		feelsLike.innerText = json.current.feelslike_c
+		wind.innerText = json.current.wind_kph
 
-
+		imgContainer.innerHTML = `<img src='${json.current.condition.icon}'/>`
+		lastUpdate.innerText = json.current.last_updated
+	}
+	)
+		
+	// .catch(err => console.error(err));
+	
+		
 
 
