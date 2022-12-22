d6aba4b285764edd4679ec9be62f941008323c66
philmeisters <philip.sawallies@wolterskluwer.com>
2022-12-22 11:57:37 +0100

    import shell for game

---
+++ modules/game.js
@@ -0,0 +1,129 @@
+/* 
+🌟 APP: Fighting Game
+
+Create an updateGame() function that will update the DOM with the state of the game 👇
+========================================
+
+- updateGame()
+
+These are the 2 classes you must create and their methods 👇
+========================================
+
+class Player {
+  - strike()
+  - heal()
+}
+
+class Game {
+  - play()
+  - checkIsOver()
+  - declareWinner()
+  - reset()
+}
+
+These functions are hard coded in the HTML. So, you can't change their names.
+
+These are all the DIV ID's you're gonna need access to 👇
+========================================================
+#1 ID 👉 'play' = Button to run simulation
+#2 ID 👉 'result' = Div that holds the winner of the match
+#3 ID 👉 'p1Name' = Div that holds player 1's Name
+#4 ID 👉 'p2Name' = Div that holds player 2's Name
+#5 ID 👉 'p1Health' = Div that holds player 1's health
+#6 ID 👉 'p2Health' = Div that holds player 2's health
+*/
+
+// ** Grabs elements from the DOM and stores them into variables **
+let playButton = document.getElementById('play')
+let resultDiv = document.getElementById('result')
+let p1NameDiv = document.getElementById('p1Name')
+let p2NameDiv = document.getElementById('p2Name')
+let p1HealthDiv = document.getElementById('p1Health')
+let p2HealthDiv = document.getElementById('p2Health')
+
+// ** Check if either players health is  0 and if it is, then update isOver to true **
+const updateGame = (p1,p2,gameState) => {
+  // Update the DOM with the names and the latest health of players
+
+  // Condition IF either player health is <= 0 then set isOver to true and declareWinner
+
+}
+
+// ** Create the Player class which can create a player with all it's attributes and methods **
+// qazi = new Player('Qazi', 100, 7)
+// qazi.name 👉 'Qazi'
+// qazi.health 👉 100
+// qazi.attackDmg 👉 7
+class Player {
+  constructor(name, health, attackDamage) {
+    this.name = name;
+    this.health = health;
+    this.attackDmg = attackDamage;
+  }
+  // ** Attack an enemy with a random number from 0 to YOUR attackDmg bonus **
+  strike (player, enemy, attackDmg) {
+    
+    // Get random number between 1 - 10 and that is damageAmount
+
+    // Subtract the enemy health with the damageAmount
+
+    //  Update the game and DOM with updateGame()
+
+    //  Return a message of 'player name attacks enemy name for damageAmount'
+
+  }
+  // ** Heal the player for random number from  1 to 5 **
+  heal (player) {
+    
+    // Get random number between 1 - 5 and store that in hpAmount
+
+    // Add hpAmount to players health
+
+    //  Update the game and DOM with updateGame()
+
+    //  Return a message of 'player name heals for hpAmount HP'
+
+  }
+}
+
+// ** Create the Game class with all it's attributes and methods to run a match **
+// game = new Game()
+// game.isOver 👉 false
+class Game {
+  constructor() {
+    this.isOver = false;
+  }
+
+  // ** If the game is over and a player has 0 health declare the winner! **
+  declareWinner(isOver,p1, p2) {
+    
+    // Create a message variable that will hold a message based on the condition
+
+    // If isOver is true AND p1 health is <= 0 then update message variable  to 'p1 WINS!'
+
+    // Else if isOver is true AND p2 health is <= 0 then update message variable  to 'p2 WINS!'
+    // Play victory sound
+
+    // Return message variable 
+
+  }
+
+  // ** Reset the players health back to it's original state and isOver to FALSE **
+  reset(p1,p2) {
+    // set p1 health and p2 health back to 100 and isOver back to false and clear resultDiv.innerText and don't forget to updateGame()
+
+  }
+  
+  // ** Simulates the whole match untill one player runs out of health **
+  play(p1, p2) {
+    // Reset to make sure player health is back to full before starting
+
+    // Make sure the players take turns untill isOver is TRUE
+    while (!this.isOver) {
+      //Make sure both players get strike() and heal() once each loop
+    }
+    // Once isOver is TRUE run the declareWinner() method 
+    
+  }
+
+}
